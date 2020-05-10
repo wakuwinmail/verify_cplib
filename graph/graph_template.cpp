bd33a1b0fb7adab360365fb900827b8f82430d5c
wakuwinmail <wakuwinmail@yahoo.co.jp>
2020-05-10 16:00:29 +0900

    change:グラフ用テンプレートを追加

--- graph/graph_template.cpp
+++ graph/graph_template.cpp
@@ -1,2 +1,14 @@
 #include <vector>
-using Graph=std::vector<std::vector<int>>;//無向グラフ
+template<typename T=long long>
+struct edge {
+public:
+    int from;
+    int to;
+    T cost;
+    edge(int from,int to,T cost):from(from),to(to),cost(cost){}
+};
+
+template<typename T>
+using graph = std::vector<std::vector<edge<T>>>;//重み付きグラフ
+
+using Graph = std::vector<std::vector<int>>;//重み無しグラフ
